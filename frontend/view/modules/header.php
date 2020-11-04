fc2ce8fdfcb0a0aa6264594ecabcb7a63328623b
Daniela Ayon <dayon.1602@gmail.com>
2020-11-04 13:56:46 -0600

    Class 2 - Adding header module view, style, top bar and header menu

---
+++ frontend/view/modules/header.php
@@ -0,0 +1,84 @@
+<div class="container-fluid topBar" id="top">
+<div class="container">
+<div class="row">
+<!-- Social  Networks -->
+<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+<ul>
+<li>
+<a href="https://facebook.com" target="_black">
+<i class="fa fa-facebook"></i>
+</a>
+</li>
+<li>
+<a href="https://youtube.com" target="_black">
+<i class="fa fa-youtube"></i>
+</a>
+</li>
+<li>
+<a href="https://instagram.com" target="_black">
+<i class="fa fa-instagram"></i>
+</a>
+</li>
+<li>
+<a href="https://twitter.com" target="_black">
+<i class="fa fa-twitter"></i>
+</a>
+</li>
+</ul>
+</div>
+
+<!-- Register  -->
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 social">
+<ul>
+<li>
+<a href="#signInModal" data-toggle="model">Sign in</a>
+</li>
+<li>|</li>
+<li>
+<a href="#createAccountModal" data-toggle="model">Create an account</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+<!-- Header  -->
+<header class="container-fluid">
+<div class="container">
+<div class="row" id="header">
+
+<!-- Logo -->
+<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logo">
+<a href="#">
+<img src="http://localhost/univa-ecommerce/backend/view/img/template/logo.png" class="img-responsive" alt="univa-ecommerse">
+</a>
+</div>
+
+<!-- Search & Categories -->
+<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+<!-- Categories button -->
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backgroundColor" id="buttonCategories">
+<p>Categories 
+<span class="pull-right">
+<i class="fa fa-bars" aria-hidden="true"></i>
+</span>
+</p>
+</div>
+
+<!-- Search -->
+<div class="input-groupcol-lg-8 col-md-8 col-sm-8 col-xs-12" id="search">
+<input type="search" name="search" class="form-control" placeholder="Search...">
+<span class="input-group-btn">
+<a href="#">
+<button class="btn btn-default backgroundColor" type="submit">
+<i class="fa fa-search" aria-hidden="true"></i>
+</button>
+</a>
+</span>
+</div>
+</div>
+</div>
+</div>
+</header>
--- frontend/view/template.php
+++ frontend/view/template.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="UNIVA - Ecommerce">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+    <meta name="keywords" content="Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit">
     <title>UNIVA - Ecommerce</title>
+    <link rel="stylesheet" href="view/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="view/css/plugins/font-awesome.min.css">
+    <script src="view/js/plugins/jquery.min.js"></script>
+    <script src="view/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>UNIVA - Ecommerce - FE</h1>
+    <?php
+    include "modules/header.php";
+    ?>
 </body>
 </html>
