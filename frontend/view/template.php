33cdc758bd3b893f97b300bf19792f13fdc11d83
José Ignacio Menchaca Castro <31707457+IgnacioCastro0713@users.noreply.github.com>
2020-11-10 15:52:16 -0600

    Update template.php

--- frontend/view/template.php
+++ frontend/view/template.php
@@ -7,15 +7,13 @@
     <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
     <meta name="keywords" content="Lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit">
     <title>UNIVA - Ecommerce</title>
-    <link rel="stylesheet" href="view/css/plugins/bootstrap.min.css">
-    <link rel="stylesheet" href="view/css/plugins/font-awesome.min.css">
-    <script src="view/js/plugins/jquery.min.js"></script>
-    <script src="view/js/plugins/bootstrap.min.js"></script>
-    
-    <!-- My CSS styles -->
-<link rel="stylesheet" href="view/css/template.css">
-<link rel="stylesheet" href="view/css/header.css">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./view/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="./view/css/plugins/font-awesome.min.css">
+  <link rel="stylesheet" href="./view/css/template.css">
+  <link rel="stylesheet" href="./view/css/header.css">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <script src="./view/js/plugins/jquery.min.js"></script>
+    <script src="./view/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
