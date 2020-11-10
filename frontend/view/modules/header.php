e36eed35859d56e885a8cf50ff0a7279683b2174
José Ignacio Menchaca Castro <31707457+IgnacioCastro0713@users.noreply.github.com>
2020-11-10 15:51:39 -0600

    Update header.php

--- frontend/view/modules/header.php
+++ frontend/view/modules/header.php
@@ -1,150 +1,143 @@
 <div class="container-fluid topBar" id="top">
-<div class="container">
-<div class="row">
-<!-- Social  Networks -->
-<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 socialNetwork">
-<ul>
-<li>
-<a href="https://facebook.com" target="_black">
-<i class="fa fa-facebook facebookWhite"></i>
-</a>
-</li>
-<li>
-<a href="https://youtube.com" target="_black">
-<i class="fa fa-youtube youtubeWhite"></i>
-</a>
-</li>
-<li>
-<a href="https://instagram.com" target="_black">
-<i class="fa fa-instagram instagramWhite"></i>
-</a>
-</li>
-<li>
-<a href="https://twitter.com" target="_black">
-<i class="fa fa-twitter twitterWhite"></i>
-</a>
-</li>
-</ul>
-</div>
+  <div class="container">
+    <div class="row">
+      <!-- Social  Networks -->
+      <div class="col-lg-9">
+        <ul>
+          <li>
+            <a href="https://facebook.com" target="_black">
+              <i class="fa fa-facebook facebookWhite"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://youtube.com" target="_black">
+              <i class="fa fa-youtube youtubeWhite"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://instagram.com" target="_black">
+              <i class="fa fa-instagram instagramWhite"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com" target="_black">
+              <i class="fa fa-twitter twitterWhite"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
 
-<!-- Register  -->
-<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registry">
-<ul>
-<li>
-<a href="#signInModal" data-toggle="model">Sign in</a>
-</li>
-<li>|</li>
-<li>
-<a href="#createAccountModal" data-toggle="model">Create an account</a>
-</li>
-</ul>
-</div>
-</div>
-</div>
+      <!-- Register  -->
+      <div class="col-lg-3">
+        <ul>
+          <li>
+            <a href="#signInModal" data-toggle="model">Sign in</a>
+          </li>
+          <li><a href="#" type="text">|</a></li>
+          <li>
+            <a href="#createAccountModal" data-toggle="model">Create an account</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
 
 <!-- Header  -->
 <header class="container-fluid">
-<div class="container">
-<div class="row" id="header">
+  <div class="container">
+    <div class="row" id="header">
 
-<!-- Logo -->
-<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logo">
-<a href="#">
-<img src="http://localhost/univa-ecommerce/backend/view/img/template/logo.png" class="img-responsive" alt="univa-ecommerse">
-</a>
-</div>
+      <!-- Logo -->
+      <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logo">
+        <a href="#">
+          <img src="http://localhost/univa-ecommerce/backend/view/img/template/logo.png" class="img-responsive"
+               alt="univa-ecommerse">
+        </a>
+      </div>
 
-<!-- Search & Categories -->
-<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+      <!-- Search & Categories -->
+      <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
 
-<!-- Categories button -->
-<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backgroundColor" id="buttonCategories">
-<p>Categories 
-<span class="pull-right">
+        <!-- Categories button -->
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backgroundColor" id="buttonCategories">
+          <p>Categories
+            <span class="pull-right">
 <i class="fa fa-bars" aria-hidden="true"></i>
 </span>
-</p>
-</div>
+          </p>
+        </div>
 
-<!-- Search -->
-<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="search">
-<input type="search" name="search" class="form-control" placeholder="Search...">
-<span class="input-group-btn">
-<a href="#">
-<button class="btn btn-default backgroundColor" type="submit">
-<i class="fa fa-search" aria-hidden="true"></i>
-</button>
-</a>
-</span>
-</div>
+        <!-- Search -->
+        <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="search">
+          <input type="search" name="search" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+          <button class="btn btn-default backgroundColor" type="submit">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+          </span>
+          <button class="btn btn-default pull-left backgroundColor right">
+            <i class="fa fa-shopping-cart"></i>
+          </button>
+        </div>
 
-<!-- Shopping car -->
-<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="shoppingCar">
-<a href="#">
-<button class="btn btn-default pull-left backgroundColor">
-<i class="fa fa-shopping-cart"></i>
-</button>
-</a>
-</div>
+      </div>
+    </div>
 
-</div>
-</div>
+    <div class="col-xs-12 backgroundColor" id="categories">
+      <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <h4>
+          <a href="#" class="pixelCategories">Lorem ipsum</a>
+          <hr>
+          <ul>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+          </ul>
+        </h4>
+      </div>
+      <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <h4>
+          <a href="#" class="pixelCategories">Lorem ipsum</a>
+          <hr>
+          <ul>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+          </ul>
+        </h4>
+      </div>
+      <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <h4>
+          <a href="#" class="pixelCategories">Lorem ipsum</a>
+          <hr>
+          <ul>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+          </ul>
+        </h4>
+      </div>
+      <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <h4>
+          <a href="#" class="pixelCategories">Lorem ipsum</a>
+          <hr>
+          <ul>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+            <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+          </ul>
+        </h4>
+      </div>
 
-<div class="col-xs-12 backgroundColor" id="categories">
-<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-<h4>
-<a href="#" class="pixelCategories">Lorem ipsum</a>
-<hr>
-<ul>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-</ul>
-</h4>
-</div>
-<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-<h4>
-<a href="#" class="pixelCategories">Lorem ipsum</a>
-<hr>
-<ul>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-</ul>
-</h4>
-</div>
-<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-<h4>
-<a href="#" class="pixelCategories">Lorem ipsum</a>
-<hr>
-<ul>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-</ul>
-</h4>
-</div>
-<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-<h4>
-<a href="#" class="pixelCategories">Lorem ipsum</a>
-<hr>
-<ul>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-<li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
-</ul>
-</h4>
-</div>
-
-</div>
-</div>
+    </div>
+  </div>
 </header>
